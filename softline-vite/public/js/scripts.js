a4bcf51cf2f2e9adfb1a8dfaddae0d05790c96aa
Vlad Samokhvalov <samokhvalov.v.i@yandex.ru>
2023-02-21 21:49:04 +0800

    Доделал адаптив

--- softline-vite/public/js/scripts.js
+++ softline-vite/public/js/scripts.js
@@ -1,37 +1,43 @@
 const toggleMenu = () => document.body.classList.toggle("open");
 
 function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-new Swiper('.swiper', {
+new Swiper(".swiper", {
   loop: true,
 
   navigation: {
-    nextEl: '.gallery-btn-next',
-    prevEl: '.gallery-btn-prev',
+    nextEl: ".gallery-btn-next",
+    prevEl: ".gallery-btn-prev",
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
-  }
+  },
 });
 
+const accordion = document.getElementsByClassName("directions-element");
 
-const accordion = document.getElementsByClassName('directions-element');
-
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('open')
-  })
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("open");
+  });
 }
+
+const dropdown = document.getElementsByClassName("submenu-item");
+
+for (i = 0; i < dropdown.length; i++)
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("open");
+  });
